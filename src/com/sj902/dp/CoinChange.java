e17962fcdf917fe8e78554ecb659336aaa8fe13c
sj902 <shubhamj@tuta.io>
2024-08-17 22:07:58 +0530

    wip

--- src/com/sj902/dp/CoinChange.java
+++ src/com/sj902/dp/CoinChange.java
@@ -1,40 +1,25 @@
 package com.sj902.dp;
 
+import java.util.Arrays;
+
 public class CoinChange {
-
-    public static void main(String[] args) {
-    }
-
-    int[][] res;
-
     public int coinChange(int[] coins, int amount) {
-        res = new int[amount + 1][coins.length + 1];
-        for (int i = 0; i <= amount; i++) {
-            for (int j = 0; j <= coins.length; j++) {
-                res[i][j] = -1;
+        int[][] res = new int[coins.length + 1][amount + 1];
+        for (int i = 0; i <= coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j == 0) {
+                    res[i][j] = 0;
+                } else if (i == 0) {
+                    res[i][j] = Integer.MAX_VALUE;
+                } else {
+                    if (j >= coins[i - 1] && res[i][j - coins[i - 1]] != Integer.MAX_VALUE) {
+                        res[i][j] = Math.min(res[i - 1][j], 1 + res[i][j - coins[i - 1]]);
+                    } else {
+                        res[i][j] = res[i - 1][j];
+                    }
+                }
             }
         }
-        int k = coinChangeAux(coins, amount, coins.length - 1);
-        return k == Integer.MAX_VALUE ? -1 : k;
-    }
-
-    public int coinChangeAux(int[] coins, int amount, int i) {
-        if (amount == 0) return 0;
-        if (amount < 0 || i < 0) return Integer.MAX_VALUE - 1;
-        if (res[amount][i] != - 1) return res[amount][i];
-
-        if (i == 0) {
-            int k = coinChangeAux(coins, amount - coins[i], i);
-            if (k == Integer.MAX_VALUE - 1) return res[amount][i] = Integer.MAX_VALUE - 1;
-            else return res[amount][i] = k + 1;
-        }
-
-        int a = coinChangeAux(coins, amount - coins[i], i);
-        int b = coinChangeAux(coins, amount, i - 1);
-        if (a != Integer.MAX_VALUE) {
-            a++;
-        }
-
-        return res[amount][i] = Math.min(a, b);
+        return res[coins.length][amount] == Integer.MAX_VALUE ? -1 : res[coins.length][amount];
     }
 }
