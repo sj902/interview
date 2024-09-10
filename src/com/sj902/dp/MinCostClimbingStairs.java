ab63a032147b11668b85700984f99b672b128899
sj902 <shubhamj@tuta.io>
2024-09-10 19:09:56 +0530

    wip

--- src/com/sj902/dp/MinCostClimbingStairs.java
+++ src/com/sj902/dp/MinCostClimbingStairs.java
@@ -1,31 +1,14 @@
 package com.sj902.dp;
 
 public class MinCostClimbingStairs {
-    int[] res;
-
     public int minCostClimbingStairs(int[] cost) {
-        res = new int[cost.length + 1];
-        if (cost.length == 1) return cost[0];
-
-
-        for (int i = 0; i <= cost.length; i++) {
-            res[i] = Integer.MAX_VALUE;
+        int[] dp = new int[cost.length + 1];
+        int n = cost.length;
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.min(cost[i - 2] + dp[i - 2], cost[i - 1] + dp[i - 1]);
         }
-
-        res[cost.length] = 0;
-        return Math.min(minCostClimbingStairsAux(cost, 0), minCostClimbingStairsAux(cost, 1));
-    }
-
-    public int minCostClimbingStairsAux(int[] cost, int i) {
-        if (i > cost.length) return Integer.MAX_VALUE;
-
-        if (res[i] != Integer.MAX_VALUE) return res[i];
-
-        if (i < cost.length) {
-            int oneStep = minCostClimbingStairsAux(cost, i + 1);
-            int twoSteps = minCostClimbingStairsAux(cost, i + 2);
-            res[i] = cost[i] + Math.min(oneStep, twoSteps);
-        }
-        return res[i];
+        return dp[n - 1];
     }
 }
