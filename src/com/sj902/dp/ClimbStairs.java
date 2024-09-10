e263e5ba2651de5a72db83116e385dcbd30c6989
sj902 <shubhamj@tuta.io>
2024-09-10 07:28:30 +0530

    wip

--- src/com/sj902/dp/ClimbStairs.java
+++ src/com/sj902/dp/ClimbStairs.java
@@ -1,13 +1,9 @@
 package com.sj902.dp;
 
 public class ClimbStairs {
-    public static void main(String[] args) {
-        System.out.println(climbStairs(3));
-    }
+    int[] res;
 
-    static int[] res;
-
-    public static int climbStarisAux(int n) {
+    public int climbStarisAux(int n) {
         if (n == 0) return 1;
         if (n == 1) return 1;
 
@@ -17,8 +13,8 @@ public class ClimbStairs {
         return res[n];
     }
 
-    public static int climbStairs(int n) {
-        res = new int[n+2];
+    public int climbStairs(int n) {
+        res = new int[n + 2];
         for (int i = 0; i <= n; i++) {
             res[i] = -1;
         }
