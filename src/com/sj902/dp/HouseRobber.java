4b8e40ac9633c2906825dd1bfb7920a13819908e
sj902 <shubhamj@tuta.io>
2024-09-21 22:04:00 +0530

    wip

--- src/com/sj902/dp/HouseRobber.java
+++ src/com/sj902/dp/HouseRobber.java
@@ -1,18 +1,14 @@
 package com.sj902.dp;
 
 public class HouseRobber {
-    Integer[] res;
 
     public int rob(int[] nums) {
-        res = new Integer[nums.length];
-        return robAux(nums, nums.length-1);
-    }
-
-    private int robAux(int[] nums, int idx) {
-        if (idx >= nums.length) return 0;
-        if (idx == 0) return nums[idx];
-        if (idx == 1) return Math.max(nums[idx], nums[idx] - 1);
-        if (res[idx] != null) return res[idx];
-        return res[idx] = Math.max(nums[idx] + robAux(nums, idx - 2), robAux(nums, idx - 1));
+        Integer[] res = new Integer[nums.length + 1];
+        res[0] = 0;
+        res[1] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res[i+1] = Math.max(res[i] , res[i-1]+nums[i]);
+        }
+        return res[nums.length];
     }
 }
